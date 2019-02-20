62b5f9ad8bb95c0038afdaacff638c7f7efa1494
Christian Cagide <ccagide1@gmail.com>
2019-02-19 21:03:01 -0600

    Avance pagina info-instructors con imagen, nombre y descripcion

---
+++ resources/views/instructor-info.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+</head>
+<body>
+
+    {{-- head --}}
+
+    {{-- Content --}}
+    <div class="container-fluid" style="background-color:#222">
+        <div class="container">
+            <div class="row">
+                <div class="image col-md-5">
+                    <img src="https://i.pinimg.com/originals/dd/46/60/dd46604477f46b3e306f5b9600cccb64.png" class="img-fluid" alt="Instructor image">
+                </div>
+                <div class="info col-md-7">
+                    <h1 style="color:aqua">Dani</h1>
+                    <div class="text-justify" style="color:white">
+                        Psicóloga en proceso, amante de los viajes y las distintas culturas, pero más importante aún es que es amante de México en todo su esplendor.
+                        Todos aquellos que conocemos a Dani podemos decir que es una persona alegre que disfruta de su día al máximo y contagia siempre a los demás de felicidad pura. Precisamente esto es lo que hace que salgas de su clase con una sonrisa de lado a lado.
+
+                        Pasiones: Bailar tap, hacer ejercicio, todo lo que tenga que ver con psicología, mantener su cuerpo y mente en equilibrio e ir a la playa.
+
+                        Drives: a Dani la motiva e inspira rodearse de gente con buena vibra, buen corazón, fortaleza y el apoyo incondicional de personas que quiere. La mueve hacer cosas que la reten, fortalezcan y la hagan crecer como persona.
+
+                        Rolando: Rolo para Dani es un tiempo clave de su día, ya que es aquello que la motiva a dar lo mejor de sí y la reta. Lo más importante para ella en su clase es compartir con todos esa alegría, ser capaz de brindarles un momento divertido, de reflexión, lleno de vida para que todos se motiven a dar lo mejor de sí en cada pedaleada.
+                        Su clase es intensa, llena de energía, retadora, reflexiva y por supuesto que no puede fallar la buena música; porque para Dani es importante ver cómo cada canción contagia a todos de una manera distinta y los impulsa a dejarlo todo ahí dentro.
+
+                        Guilty pleasure: Sin duda es el chocolate y el pollo a la naranja de Qin jajaja
+
+                        Máquina del tiempo: Los años 30’s sería una época en la que le gustaría haber vivido ya que ama el estilo de baile que caracterizaba a esta época (el swing) y le fascina la forma tan peculiar y elegante en la que se vestían.
+                    </div>
+                    <div class="phrase" style="color:white">
+                        "Busca lo que encienda tu alma."
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="location-date col-md-7">
+                    <select name="Places" class="form-control w-50">
+                        <option selected>Providencia</option>
+                        <option value="Lugar1">Lugar 1</option>
+                        <option value="Lugar2">Lugar 2</option>
+                    </select>
+                </div>
+
+                <div class="music col-md-5">
+                    Music
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/123', function () {
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/instructor-info', function () {
+    return view('instructor-info');
+});
