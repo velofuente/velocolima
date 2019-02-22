ded6013433ad2a5f27798f4bc9a440328c9b10e3
Christian Cagide <ccagide1@gmail.com>
2019-02-21 20:25:13 -0600

    fixed conflict

---
+++ resources/views/first-visit.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>First Visit</title>
+</head>
+<body>
+
+    <div class="container container-fluid">
+        <div class="row">
+            <div></div>
+        </div>
+    </div>
+
+</body>
+</html>
