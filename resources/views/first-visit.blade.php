14c875ea6ebe23b61591cbc2e1047edca6f7d7b1
Christian Cagide <ccagide1@gmail.com>
2019-02-22 20:55:48 -0600

    Added the file for the page first-visit

---
+++ resources/views/first-visit.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('title')
+    Rolo | Primera visita
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+
+            </div>
+        </div>
+    </div>
+@endsection
