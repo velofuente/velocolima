8391ea297a40339df67b896bce1a4635675f4871
Pablo Garcìa <pjimenez0@ucol.mx>
2019-02-25 20:20:45 -0600

    card class fixed in login, register, verify and home

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card bg-dark">
-                <div class="card-header">{{ __('Registro') }}</div>
+            <div>
+                    <h3 class="mx-auto">Registro</h3>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
