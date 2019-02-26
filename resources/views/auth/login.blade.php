78f8d6b969a35c1a648f5c9364704460c0196cf6
Pablo Garcìa <pjimenez0@ucol.mx>
2019-02-25 19:37:16 -0600

    login & register completed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.app')
+@extends('layout')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card bg-dark">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
