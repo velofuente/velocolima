9ab8697049004a218d1f5e6a349fcafd38aca275
Ricardo Mendoza <ricardoww10@hotmail.com>
2019-02-28 22:00:03 -0600

    modal created

--- public/js/layout-scripts.js
+++ public/js/layout-scripts.js
@@ -5,3 +5,11 @@ function classQuantity(x) {
         document.getElementById("exampleModalLongTitle").innerHTML = "Comprar "+x+" clase";
     }
 }
+
+function classQuantity(x) {
+    if (x >= 3) {
+        document.getElementById("exampleModalLongTitle").innerHTML = "Comprar "+x+" clases";
+    } else {
+        document.getElementById("exampleModalLongTitle").innerHTML = "Comprar "+x+" clase";
+    }
+}
