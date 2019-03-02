3efe46a2c38c2b53ed07f197e3b091b94c2428c2
Ricardo Mendoza <ricardoww10@hotmail.com>
2019-03-01 21:59:00 -0600

    starting functionality of bike-selection

--- resources/views/bike-selection.blade.php
+++ resources/views/bike-selection.blade.php
@@ -19,24 +19,24 @@ Reservar Bici
                     <p class="bikes">2</p>
                     <p class="bikes">3</p>
                     <p class="bikes">4</p>
-                    <p class="selected">5</p>
+                    <p class="bikes">5</p>
                     <p class="bikes">6</p>
                     <p class="bikes">7</p>
                 </div>
                 <div class="col">
-                    <p class="selected">8</p>
-                    <p class="selected">9</p>
-                    <p class="selected">10</p>
+                    <p class="bikes">8</p>
+                    <p class="bikes">9</p>
+                    <p class="bikes">10</p>
                     <p class="bikes">11</p>
                     <p class="bikes">12</p>
-                    <p class="selected">13</p>
+                    <p class="bikes">13</p>
                     <p class="bikes">14</p>
                 </div>
                 <div class="col">
                     <p class="bikes">15</p>
                     <p class="bikes">16</p>
                     <p class="bikes">17</p>
-                    <p class="selected">18</p>
+                    <p class="bikes">18</p>
                     <p class="bikes">19</p>
                     <p class="bikes">20</p>
                     <p class="bikes">21</p>
@@ -44,17 +44,17 @@ Reservar Bici
                 <div class="col">
                     <p class="bikes">22</p>
                     <p class="bikes">23</p>
-                    <p class="selected">24</p>
+                    <p class="bikes">24</p>
                     <p class="bikes">25</p>
                     <p class="bikes">26</p>
-                    <p class="selected">27</p>
+                    <p class="bikes">27</p>
                     <p class="bikes">28</p>
                 </div>
                 <div class="col">
                     <p class="bikes">29</p>
-                    <p class="selected">30</p>
-                    <p class="selected">31</p>
-                    <p class="selected">32</p>
+                    <p class="bikes">30</p>
+                    <p class="bikes">31</p>
+                    <p class="bikes">32</p>
                     <p class="bikes">33</p>
                     <p class="bikes">34</p>
                     <p class="bikes">35</p>
@@ -62,9 +62,9 @@ Reservar Bici
                 <div class="col">
                     <p class="bikes">36</p>
                     <p class="bikes">37</p>
-                    <p class="selected">38</p>
+                    <p class="bikes">38</p>
                     <p class="bikes">39</p>
-                    <p class="selected">40</p>
+                    <p class="bikes">40</p>
                     <p class="bikes">41</p>
                     <p class="bikes">42</p>
                 </div>
@@ -89,10 +89,13 @@ Reservar Bici
                     </div>
                     <div>
                         <h5 class="first">NO. DE LUGAR</h5>
-                        <h5>--</h5>
+                        <h5 id="placeNum">--</h5>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+@section('extraScripts')
+    <script src="js/bike-selection-script.js"></script>
 @endsection
