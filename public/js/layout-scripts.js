0b512d1b665c0b52956aebe7937147f1d45251dd
Ricardo Mendoza <ricardoww10@hotmail.com>
2019-03-01 20:09:40 -0600

    buy redirection complete

--- public/js/layout-scripts.js
+++ public/js/layout-scripts.js
@@ -1,10 +1,3 @@
-document.getElementById("buy").onclick = function() {packages()};
-var packs = document.getElementById("packages");
-function packages() {
-    window.location.href="/home";
-    packs.scrollIntoView();
-}
-
 function classQuantity(x) {
     if (x >= 3) {
         document.getElementById("exampleModalLongTitle").innerHTML = "Comprar "+x+" clases";
