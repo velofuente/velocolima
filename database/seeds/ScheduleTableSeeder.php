85493c8e6fe8be973d081d6e8cdc2150c9faefe2
Paul Moran <paulpalemonmoran@gmail.com>
2019-03-07 18:09:52 -0600

    Schedule Seeders Modified

--- database/seeds/ScheduleTableSeeder.php
+++ database/seeds/ScheduleTableSeeder.php
@@ -13,8 +13,8 @@ class ScheduleTableSeeder extends Seeder
     public function run()
     {
         $schedule = new Schedule();
-        $schedule->day = "2019-03-05";
-        $schedule->hour = "16:00:00";
+        $schedule->day = "2019-03-07";
+        $schedule->hour = "10:00:00";
         $schedule->instructor_id = "1";
         $schedule->class_id = "1";
         $schedule->room_id = "1";
@@ -31,11 +31,65 @@ class ScheduleTableSeeder extends Seeder
         $schedule->save();
 
         $schedule = new Schedule();
+        $schedule->day = "2019-03-09";
+        $schedule->hour = "12:00:00";
+        $schedule->instructor_id = "3";
+        $schedule->id_classes = "1";
+        $schedule->id_rooms = "1";
+        $schedule->reservation_limit = "5";
+        $schedule->save();
+
+        $schedule = new Schedule();
+        $schedule->day = "2019-03-10";
+        $schedule->hour = "18:00:00";
+        $schedule->instructor_id = "4";
+        $schedule->id_classes = "1";
+        $schedule->id_rooms = "1";
+        $schedule->reservation_limit = "5";
+        $schedule->save();
+
+        $schedule = new Schedule();
+        $schedule->day = "2019-03-11";
+        $schedule->hour = "16:00:00";
+        $schedule->instructor_id = "5";
+        $schedule->id_classes = "1";
+        $schedule->id_rooms = "1";
+        $schedule->reservation_limit = "5";
+        $schedule->save();
+
+        $schedule = new Schedule();
+        $schedule->day = "2019-03-12";
+        $schedule->hour = "10:00:00";
+        $schedule->instructor_id = "6";
+        $schedule->id_classes = "1";
+        $schedule->id_rooms = "1";
+        $schedule->reservation_limit = "5";
+        $schedule->save();
+
+        $schedule = new Schedule();
+        $schedule->day = "2019-03-13";
+        $schedule->hour = "10:00:00";
+        $schedule->instructor_id = "7";
+        $schedule->id_classes = "1";
+        $schedule->id_rooms = "1";
+        $schedule->reservation_limit = "5";
+        $schedule->save();
+
+        $schedule = new Schedule();
         $schedule->day = "2019-03-07";
         $schedule->hour = "08:00:00";
         $schedule->instructor_id = "1";
         $schedule->class_id = "1";
         $schedule->room_id = "1";
+        $schedule->reservation_limit = "5";
+        $schedule->save();
+
+        $schedule = new Schedule();
+        $schedule->day = "2019-03-08";
+        $schedule->hour = "10:00:00";
+        $schedule->instructor_id = "9";
+        $schedule->id_classes = "1";
+        $schedule->id_rooms = "1";
         $schedule->reservation_limit = "5";
         $schedule->save();
     }
