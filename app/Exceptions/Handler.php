dcd75c0b69254a2c32cda1d39d55c9afb3d4bb8d
Pablo Garcìa <pjimenez0@ucol.mx>
2019-03-11 19:36:19 -0600

    User functionallity: add card, change password and edit personal data finished

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceof \Illuminate\Auth\AuthenticationException){
+            return redirect('/welcome')->with('flash', 'Por favor inicia sesion');
+        }
         return parent::render($request, $exception);
     }
 }
