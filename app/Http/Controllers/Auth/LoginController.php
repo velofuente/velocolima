b82307b919d50b8bd72634491e56e018e992c6b1
Pablo Garcìa <pjimenez0@ucol.mx>
2019-03-11 19:36:19 -0600

    User functionallity: add card, change password and edit personal data finished

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,39 +1,40 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->middleware('guest', ['only' => 'showLoginForm']);
+    }
+    public function showLoginForm()
+    {
+        return  view('auth.login');
+    }
+    public function login()
+    {
+        $credentials = $this->validate(request(),[
+            'email' => 'email|required|string',
+            'password' => 'required|string'
+        ]);
+        //return $credentials;
+        if(Auth::attempt($credentials))
+        {
+            return redirect()->route('user.index');
+        }
+
+        return back()
+            ->withErrors(['email' => trans('auth.failed')])
+            ->withInput(request(['email']));
+    }
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect('/');
     }
 }
