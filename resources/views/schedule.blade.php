452c277d7694108584b00a3f6c1544d9f84bc2f1
Paul Moran <paulpalemonmoran@gmail.com>
2019-03-12 18:11:38 -0600

    Minor modifications and fixes

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -17,7 +17,7 @@
                     <div class="row">
                         <div class="col-sm-3">
                             <div class="dropdown">
-                                <select class="dropdown" style="width: 90%" href="#" role="button" id="btnScheduleInstructor" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <select class="dropdown" style="width: 90%" data-dependent="" role="button" id="ScheduleInstructor" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <option value="AllInstructors" selected="selected">Instructor</option>
                                     @foreach ($instructors as $instructor)
                                         <option value="{{$instructor->id}}">{{$instructor->name}}</option>
@@ -27,9 +27,10 @@
                         </div>
                         {{-- Branch Dropdown --}}
                         <div class="dropdown col-sm-9 d-md-flex">
-                            <select class="dropdown" style="width: 30%" href="#" role="button" id="btnScheduleInstructor" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <select class="dropdown" style="width: 30%" data-dependent="" role="button" id="ScheduleBranch" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <option value="Sucursal">Sucursal</option>
                                 @foreach ($branches as $branch)
-                                    <option value="{{$branch->name}}">{{$branch->name}}</option>
+                                    <option value="{{$branch->id}}">{{$branch->name}}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -71,5 +72,9 @@
 @endsection
 
 @section('extraScripts')
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+@endsection
     <script src="{{asset('js/schedule-script.js')}}"></script>
 @endsection
