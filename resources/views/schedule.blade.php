2eaae4857ab268c0288260d970ff6cefc1aaded6
Paul Moran <paulpalemonmoran@gmail.com>
2019-03-14 18:27:53 -0600

    Fixed hideSchedule function

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -17,10 +17,10 @@
                     <div class="row">
                         <div class="col-sm-3">
                             <div class="dropdown">
-                                <select class="dropdown" style="width: 90%" data-dependent="" role="button" id="ScheduleInstructor" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <option value="AllInstructors" selected="selected">Instructor</option>
+                                <select class="dropdown" style="width: 90%" data-dependent="" role="button" id="ScheduleInstructor" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onchange="hideSchedules()">
+                                    <option value="allInstructors" selected="selected">Instructor</option>
                                     @foreach ($instructors as $instructor)
-                                        <option value="{{$instructor->id}}">{{$instructor->name}}</option>
+                                        <option value="{{$instructor->name}}">{{$instructor->name}}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -30,7 +30,7 @@
                             <select class="dropdown" style="width: 30%" data-dependent="" role="button" id="ScheduleBranch" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <option value="Sucursal">Sucursal</option>
                                 @foreach ($branches as $branch)
-                                    <option value="{{$branch->id}}">{{$branch->name}}</option>
+                                    <option value="{{$branch->name}}">{{$branch->name}}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -54,9 +54,11 @@
                             @foreach ($schedules as $schedule)
                                 @if ($schedule->day == $today->format('Y-m-d'))
                                     <section>
-                                        <li class="scheduleItem">
-                                            <a href="/bike-selection/{{$schedule->id}}" class="scheduleItemText">{{$schedule->instructor->name}}</a>
-                                            <a href="/bike-selection/{{$schedule->id}}" class="scheduleItemText">{{ date('g:i A', strtotime($schedule->hour)) }}</a>
+                                    <li class="scheduleItem" id="{{$schedule->instructor->name}}">
+                                        <p class="scheduleItemText">
+                                                {{$schedule->instructor->name}}
+                                        </p>
+                                        <p class="scheduleItemText">{{ date('g:i A', strtotime($schedule->hour)) }}</p>
                                         </li>
                                     </section>
                                 @endif
@@ -68,10 +70,32 @@
                 </div>
             </div>
         </div>
+    <script>
+        function hideSchedules() {
+            var selectInstructor = document.getElementById("ScheduleInstructor").value;
+            var scheduleBox = document.getElementsByClassName("scheduleItem");
+
+            if (selectInstructor == "allInstructors"){
+                for (let item of scheduleBox){
+                    item.style.display = 'block';
+                }
+            } else {
+                for (let item of scheduleBox){
+                    if (selectInstructor === item.id){
+                        item.style.display = 'block';
+                    }
+                    else {
+                        item.style.display = 'none';
+                    }
+                }
+            }
+        }
+    </script>
     </body>
 @endsection
 
 @section('extraScripts')
+    <script src="js/hideSchedule.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
