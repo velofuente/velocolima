4fada7281a2300816d2b1fca15db9b59d1104232
Ricardo Mendoza <ricardoww10@hotmail.com>
2019-03-22 22:04:21 -0600

    changes on bike-selection and footer

--- public/js/bike-selection-script.js
+++ public/js/bike-selection-script.js
@@ -20,7 +20,9 @@ var splitDate = date.split(" ");
 var day = splitDate[0].charAt(0).toUpperCase();
 var month = splitDate[3].charAt(0).toUpperCase();
 
-var modifiedDate = day + splitDate[0].slice(1) +" "+ splitDate[1]+" "+ splitDate[2]+" "+ month + splitDate[3].slice(1) +" "+ splitDate[4]+" "+ splitDate[5];
+var modifiedDate = day + splitDate[0].slice(1) +" "+ splitDate[1]+" "+ splitDate[2]+" "+ month + splitDate[3].slice(1) +" ";
 document.getElementById('date').innerHTML = modifiedDate;
 
-
+$('#profilePic').click(function(){
+    document.location.href = "/instructors";
+});
