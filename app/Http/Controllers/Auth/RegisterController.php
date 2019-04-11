467f337af957b8dc098281df97cf871e09a7001a
Ricardo Mendoza <ricardoww10@hotmail.com>
2019-04-11 18:28:01 -0500

    bye siclo, welcome velo

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -120,4 +120,29 @@ class RegisterController extends Controller
     \Mail::to($user->email)->send(new VerifyMail($user));
     return $user;
     }
+
+    public function verifyUser($token)
+    {
+    $verifyUser = VerifyUser::where('token', $token)->first();
+    if(isset($verifyUser) ){
+        $user = $verifyUser->user;
+        if(!$user->verified) {
+            $verifyUser->user->verified = 1;
+            $verifyUser->user->save();
+            $status = "Your e-mail is verified. You can now login.";
+        } else {
+            $status = "Your e-mail is already verified. You can now login.";
+        }
+    } else {
+        return redirect('/login')->with('warning', "Sorry your email cannot be identified.");
+    }
+    return redirect('/login')->with('status', $status);
+    }
+    
+    protected function registered(Request $request, $user)
+    {
+    $this->guard()->logout();
+    return redirect('/login')->with('status', 'We sent you an activation code. Check your email and click on the link to verify.');
+    }
+
 }
