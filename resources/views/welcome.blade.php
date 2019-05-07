00bdb2ffb581bdd840bea834cdead3337b163b4e
Ricardo Mendoza <jmendoza20@ucol.mx>
2019-05-06 22:18:11 -0500

    homepage created, responsive design in progress

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,11 @@
   Home
 @endsection
 @section('content')
-@include('packages')
+<div class="main">
+    <img src="img/homepage/1.jpg" alt="principal" width="100%" height="50%">
+    <img class="icon" src="img/iconos/ICONO_O.png" alt="">
+    <img class="veloBrand" src="img/iconos/LOGO.png" alt="">
+    <img class="music" src="img/iconos/FRASEMUSICA.png" alt="">
+    <img class="imusic" src="img/iconos/ICONO_MUSIC.png" alt="">
+</div>
 @endsection
