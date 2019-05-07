91dbd3dd0538e92dbac9bef0ae88da945be5a908
pablokraken <pjimenez0@ucol.mx>
2019-05-06 20:25:06 -0500

    homepage functionality

--- resources/views/packages.blade.php
+++ resources/views/packages.blade.php
@@ -10,47 +10,29 @@
                 <h5 class="class f-class">CLASE</h5>
             </div>
         </div>
-        <div class="content-normal col col-sm col-md col-lg" style=" padding: 0;">
-            <div id="content-normal" class="content-n" data-toggle="modal" data-target="#exampleModalCenter" onclick="classQuantity('1')">
-                <h3 id="amount2">1</h3>
-                <h5 class="class">CLASE</h5>
-                <p class="precio">$150</p>
-                <p class="exp">Expira: 30 días</p>
-            </div>
-        </div>
-        <div class="content-normal col col-sm col-md col-lg" style=" padding: 0;">
-            <div id="content-normal" class="content-n" data-toggle="modal" data-target="#exampleModalCenter" onclick="classQuantity('5')">
-                <h3 id="amount3">5</h3>
-                <h5 class="class">CLASES</h5>
-                <p class="precio">$150</p>
-                <p class="exp">Expira: 60 días</p>
-            </div>
-        </div>
-        <div class="content-normal col col-sm col-md col-lg" style=" padding: 0;">
-            <div id="content-normal" class="content-n" data-toggle="modal" data-target="#exampleModalCenter" onclick="classQuantity('10')">
-                <h3 id="amount4">10</h3>
-                <h5 class="class">CLASES</h5>
-                <p class="precio">$150</p>
-                <p class="exp">Expira: 120 días</p>
-            </div>
-        </div>
-        <div class="content-normal col col-sm col-md col-lg" style=" padding: 0;">
-            <div id="content-normal" class="content-n" data-toggle="modal" data-target="#exampleModalCenter" onclick="classQuantity('25')">
-                <h3 id="amount5">25</h3>
-                <h5 class="class">CLASES</h5>
-                <p class="precio">$150</p>
-                <p class="exp">Expira: 180 días</p>
-            </div>
-        </div>
-        <div class="content-normal col col-sm col-md col-lg" style=" padding: 0;">
-            <div id="content-normal" class="content-n" data-toggle="modal" data-target="#exampleModalCenter" onclick="classQuantity('50')">
-                <h3 id="amount6">50</h3>
-                <h5 class="class">CLASES</h5>
-                <p class="precio">$150</p>
-                <p class="exp">Expira: 365 días</p>
-            </div>
-        </div>
-
+        @php
+            $amount=2;
+            $flag=true;
+        @endphp
+        @foreach ($products as $product)
+            @if ($product != $products{0})
+                {{--dd($products{0})--}}
+                <div class="content-normal col col-sm col-md col-lg" style=" padding: 0;">
+                    <div id="content-normal" class="content-n" data-toggle="modal" data-target="#exampleModalCenter" onclick="classQuantity('{{ --$amount }}')">
+                        <h3 id="amount{{$amount}}">{{$product->n_classes}}</h3>
+                        @if ($flag)
+                            <h5 class="class">CLASE</h5>
+                            {{$flag=false}}
+                        @else
+                            <h5 class="class">CLASES</h5>
+                        @endif
+                        <p class="precio">{{$product->price}}</p>
+                        <p class="exp">Expira: {{$product->expiration_date}} días</p>
+                    </div>
+                </div>
+                {{$amount++}}
+            @endif
+        @endforeach
         <!-- Modal -->
         @guest
         <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
