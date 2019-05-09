f7663833558024577979aa300f471dc350b86447
Ricardo Mendoza <jmendoza20@ucol.mx>
2019-05-09 12:37:46 -0500

    guest modal and some buttons changed

--- resources/views/packages.blade.php
+++ resources/views/packages.blade.php
@@ -57,7 +57,7 @@
         @guest
         <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content bg-dark">
+                <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLongTitle"></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -70,12 +70,12 @@
                         <p id="oName">Ingresa a tu Cuenta</p>
                     </div>
                     <div class="col-7">
-                        <a id="btnLog" href="{{ route('login') }}" class="btn ">INICIAR SESIÓN</a>
+                        <a id="btnLog" href="{{ route('login') }}" class="button">INICIAR SESIÓN</a>
                     </div>
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                    <button type="button" class="closeBtn" data-dismiss="modal">Cerrar</button>
                 </div>
                 </div>
             </div>
