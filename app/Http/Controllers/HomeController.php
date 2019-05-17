9bfc5e105066ace2982c31e371b3716b3416663d
Pablo Miguel Jimenez García <pjimenez0@ucol.mx>
2019-05-17 14:02:16 -0500

    avance

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\Branch;
 
 class HomeController extends Controller
 {
@@ -25,6 +26,7 @@ class HomeController extends Controller
     public function index()
     {
         $products = Product::all();
-        return view('welcome', compact('products'));
+        $branches = Branch::all();
+        return view('welcome', compact('products', 'branches'));
     }
 }
