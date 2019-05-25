0cbc823eff8f70c23b1f3e26fecd4cdf58c4bda6
Pablo Miguel Jimenez García <pjimenez0@ucol.mx>
2019-05-25 12:43:53 -0500

    branches fixed

--- resources/views/branches.blade.php
+++ resources/views/branches.blade.php
@@ -4,21 +4,6 @@
     Sucursales
 @endsection
 @section('extraStyles')
-    <style>
-    .marco {
-        height: 264px;
-        background-size: cover;
-        background-position: center;
-        background-blend-mode: multiply;
-        color: #fff;
-        position: relative;
-        border-radius: 3px;
-        margin: 20px;
-    }
-    .margin-topMain {
-        padding-top: 100px !important;
-    }
-    </style>
     <link rel="stylesheet" href="{{asset('css/branches-styles.css')}}">
 @endsection
 @section('content')
@@ -50,18 +35,19 @@
                         </div>
                     </div>
                 </div> --}}
-                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-3 border border">
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 border border mx-auto">
                     <div class="row">
-                        <div class="col-8 border border">
+                        <div class="col-6 border border-light">
                             <p class="branchName">{{$branch->name}}</p>
                             <span class="branchInfo">{{$branch->address}}</span>
                             {{-- <p>{{$branch->phone}}</p> --}}
-                            <span class="branchInfo">312XXXXXXX</span>
+                            <p class="branchInfo">312XXXXXXX</p>
                             {{-- <button class="button-primary">Reservar</button> --}}
-                            <p> <a class="btnReservar" href="{{url("/schedule")}}">Reservar</a> </p>
+                            <p class="text-center mx-auto"> <a class="btnReservar" href="{{url("/schedule")}}">Reservar</a> </p>
                         </div>
-                        <div class="col-4 border border justify-content-center">
-                            <img src="{{asset('img/iconos/CroppedLogo.png')}}" id="welcomeLogo" class="mx-auto text-center">
+                        <div class="col-6 border border justify-content-center">
+                            <img src="{{asset('/img/footer/3.jpg')}}" class="img-fluid " id="branchImage">
+                            {{-- <img src="{{asset('img/iconos/CroppedLogo.png')}}" id="welcomeLogo" class="mx-auto text-center"> --}}
                         </div>
                     </div>
                 </div> --}}
