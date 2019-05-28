7e1644ebaca2585da3a4b1831a5f9a48ab0a0695
Paul Moran <paulpalemonmoran@hotmail.com>
2019-05-28 16:17:20 -0500

    meta added to the head of layout.blade

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     {{-- {{ csrf_token() }} --}}
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
 </head>
     @include('header')
     <body>
