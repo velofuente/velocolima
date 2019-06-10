49b4330b8036a85e574255e6d2ed94b26d723d3b
Pablo Miguel Jimenez García <pjimenez0@ucol.mx>
2019-06-10 11:03:49 -0500

    prueba para PR

--- app/Http/Controllers/BranchesController.php
+++ app/Http/Controllers/BranchesController.php
@@ -9,6 +9,7 @@ class BranchesController extends Controller
 {
     public function index()
     {
+        //obtiene todas las sucursales
         $branches = Branch::all();
         return view('branches',compact('branches'));
     }
