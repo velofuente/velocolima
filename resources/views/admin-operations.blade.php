6db4658f0584771bd0c5e73637c965959b328d1c
Paul Moran <paulpalemonmoran@hotmail.com>
2019-06-14 18:05:38 -0500

    Work In Progress

--- resources/views/admin-operations.blade.php
+++ resources/views/admin-operations.blade.php
@@ -2,15 +2,15 @@
 <div class="row text-center mx-0 py-3">
     <h3 class="col-4">Operaciones</h3>
     <div class="dropdown col-8">
-        <button class="btn btn-secondary mx-3 dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Horarios
+        <button class="btn btn-success mx-3 dropdown-toggle" id="dropdownSchedule" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span id="selectedSchedule">Horario</span>
         </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <div class="dropdown-menu" aria-labelledby="dropdownSchedule">
             @foreach ($schedules as $schedule)
                 @if ($schedule->day == date('Y-m-d'))
                     @if ($schedule->hour >= date('H:i:s'))
-                        <a class="dropdown-item" href="#">
-                            <span>{{ date('g:i A', strtotime($schedule->hour)) }}</span>
+                        <a class="dropdown-item" href="javascript:showClients({{$schedule->id}})" id="{{$schedule->id}}">
+                            <span>{{ date('g:i A', strtotime($schedule->hour)) }} |</span>
                             <span>{{$schedule->instructor->name}}</span>
                             {{-- <img width="60%" height="60%" src="{{ asset('img/instructors/' . $schedule->instructor->name . '-Head.png') }}" alt=""> --}}
                         </a>
@@ -26,6 +26,7 @@
             @endforeach
         </div>
     </div>
+
 </div>
 
 {{-- Bike Grid & Table of Users--}}
@@ -46,7 +47,7 @@
                 </thead>
                 <tbody>
                     @foreach ($userSchedules as $userSchedule)
-                        <tr style="font-size: 0.9em;">
+                        <tr style="font-size: 0.9em;" id="tableBodyRow" value="{{$userSchedule->schedule_id}}">
                             <td>{{$userSchedule->user->name}}</td>
                             <td>{{$userSchedule->user->last_name}}</td>
                             <td>{{$userSchedule->user->email}}</td>
@@ -61,41 +62,35 @@
     </div>
 </div>
 
-{{-- Table  --}}
-{{-- @if (count($schedules) > 0)
-    <table class="table table-striped table-hover">
-        <thead style="font-size: 1em;">
-            <tr style="font-size: 1em;">
-                <th scope="col">ID</th>
-                <th scope="col">Día</th>
-                <th scope="col">Hora</th>
-                <th scope="col">Límite de Reservación</th>
-                <th scope="col">Instructor</th>
-                <th scope="col">Sucursal</th>
-                <th scope="col" colspan="2" class="text-center">Acción</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($schedules as $schedule)
-                <tr style="font-size: 0.9em;">
-                    <td>{{$schedule->id}}</td>
-                    <td>{{$schedule->day}}</td>
-                    <td>{{$schedule->hour}}</td>
-                    <td>{{$schedule->reservation_limit}}</td>
-                    <td>{{$schedule->instructor->name}}</td>
-                    <td>{{$schedule->branch->name}}</td>
-                    <td><button class="btn btn-primary btn-sm editProduct" id="editProduct-{{ $schedule->id }}" value="{{$schedule->id}}" data-myid="{{ $schedule->id }}" data-myday="{{ $schedule->day }}" data-toggle="modal" data-target="#editProductModal">Editar</button></td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-@else
-    <h2 class="text-center">No hay Productos Agregados</h2>
-@endif --}}
-
 {{-- Add, Delete & Edit Products Scripts --}}
 <script>
+    var variable1 = null;
+    var variable2 = null;
+    var variable3 = null;
+    var cols = null;
+
     $(document).ready(function (){
-        var product_id = null;
+        $('#main-bikes').hide();
+
+        // Dropdown Selected Option
+        $('.dropdown-menu a').click(function(){
+            $('#selectedSchedule').text($(this).text());
+        });
+
+        cols = document.querySelectorAll('#tableBodyRow');
+        variable3 = document.getElementById('tableBodyRow');
+
+        // for (let item of cols){
+        //       item.style.display = 'block';
+        // }
+        console.log(variable3);
+        console.log(cols);
     })
+
+    function showClients(id){
+        variable1 = id;
+        variable2 = document.getElementById("tableBodyRow");
+        console.log('ID guardado (Schedule): ' + id);
+        $('#main-bikes').show();
+    }
 </script>
