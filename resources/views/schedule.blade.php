0325f432df04b5c82c32e43c1885a178cfd81f8c
Paul Moran <paulpalemonmoran@hotmail.com>
2019-06-21 09:12:00 -0500

    schedule days fixed

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -24,9 +24,11 @@
             <div class="col-xs-4 col-sm-4 col-md-4 col-lg-3">
                 <input type="hidden" name="timezoneSet" value="{{setlocale(LC_TIME,'es_MX.utf8')}}">
                 <input type="hidden" name="WeekShown" value="{{$weekShown=now()}}">
+                {{-- <input type="hidden" name="setMonth" value="{{$month=strftime('%B', strtotime($weekShown)) . "+1 month")}}"> --}}
                 <input type="hidden" name="setMonth" value="{{$month=strftime('%B', strtotime($weekShown))}}">
                 <span class="weekShown">
-                    del {{date('d')}} al {{date('d', strtotime($weekShown->modify("+12 days")))}} de {{$month}}
+                    {{-- del {{date('d', strtotime($weekShown->modify("+6 days")))}} al {{date('d', strtotime($weekShown->modify("+6 days")))}} de {{$month}} --}}
+                    del {{date('d', strtotime($weekShown->modify("+5 days")))}} al {{date('d', strtotime($weekShown->modify("+6 days")))}} de JULIO
                 </span>
             </div>
             {{-- Empty Section at the Middle of the NavBar --}}
@@ -54,7 +56,7 @@
 
         {{-- Schedule Section --}}
         <input type="hidden" name="timezoneSet" value="{{date_default_timezone_set('America/Mexico_City')}}">
-        <input type="hidden" name="actualDay" value="{{$today=now()->modify('+6 days')}}">
+        <input type="hidden" name="actualDay" value="{{$today=now()->modify('+5 days')}}">
         <input type="hidden" name="thisDay" value="{{$thisDay=now()}}">
         <div class="container" name="calendar">
         @if(count($schedules) > 0)
