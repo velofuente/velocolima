9dc79129565e7855c910436b6d70d8cf7584e574
Paul Moran <paulpalemonmoran@hotmail.com>
2019-06-27 14:11:45 -0500

    Packages visual changes

--- resources/views/packages.blade.php
+++ resources/views/packages.blade.php
@@ -23,7 +23,6 @@
         @endphp
         @foreach ($products as $product)
             @if ($product != $products{0} && $product->type != "Deals" && $product->status == 1)
-                {{-- {{dd($product)}} --}}
                 {{-- <div class="content-normal pickClass" style=" margin-left: 10px; min-width: 190px !important; min-height: 280px !important;" id="prod-{{$product->id}}"> --}}
                 <div class="content-normal pickClass mx-2" id="prod-{{$product->id}}">
                     @guest
