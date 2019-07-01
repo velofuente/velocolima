3e4870e2896dd251f03953fe71fb12a29fafeff4
Paul Palemon Moran Garcia <paulpalemon@MacBook-Pro-de-Sergio.local>
2019-07-01 18:27:33 -0500

    BookClassController modified

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
             return $query->whereRaw("DATE(created_ad) >= CURRENT_DATE()")->get();
         })->where('user_id', "{$requestUser->id}")->where('status', 'active')->get();
         */
-        $bookedClasses = UserSchedule::with("schedule.instructor", "schedule.room", "schedule")->where('user_id', "{$requestUser->id}")->whereRaw("created_at >= NOW()")->where('status', 'active')->get();
+        $bookedClasses = UserSchedule::with("schedule.instructor", "schedule.room", "schedule")->where('user_id', "{$requestUser->id}")->where('status', 'active')->get();
         $previousClasses = UserSchedule::where('user_id', "{$requestUser->id}")->whereNotIn("status", ["active"])->whereRaw("created_at < NOW()")->get();
         $UserWaitLists = UserWaitList::with('waitList.schedule.instructor', 'waitList.schedule')->where('user_id', "{$requestUser->id}")->get();
         return view('user', compact('cards', 'purchaseHistory', 'classes', 'previousClasses', 'UserWaitLists', 'bookedClasses'));
