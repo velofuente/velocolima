1306b5d7a8a032a9d45050a6bf716f6cf81f1f48
Paul Palemon Moran Garcia <paulpalemon@MacBook-Pro-de-Sergio.local>
2019-07-01 17:34:54 -0500

    bike number on user nextClasses fixed

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -170,7 +170,53 @@
                                                             <tr>
                                                                 <td>{{ date('d-M-Y', strtotime($bookedClass->schedule->day)) }}</td>
                                                                 <td>{{ date('h:i A', strtotime($bookedClass->schedule->hour)) }}</td>
-                                                                <td>{{ $bookedClass->bike }}</td>
+                                                                @switch($bookedClass->bike)
+                                                                    @case(2)
+                                                                        <td>1</td>
+                                                                        @break
+                                                                    @case(10)
+                                                                        <td>2</td>
+                                                                        @break
+                                                                    @case(12)
+                                                                        <td>3</td>
+                                                                        @break
+                                                                    @case(14)
+                                                                        <td>4</td>
+                                                                        @break
+                                                                    @case(20)
+                                                                        <td>5</td>
+                                                                        @break
+                                                                    @case(22)
+                                                                        <td>6</td>
+                                                                        @break
+                                                                    @case(24)
+                                                                        <td>7</td>
+                                                                        @break
+                                                                    @case(26)
+                                                                        <td>8</td>
+                                                                        @break
+                                                                    @case(30)
+                                                                        <td>9</td>
+                                                                        @break
+                                                                    @case(32)
+                                                                        <td>10</td>
+                                                                        @break
+                                                                    @case(36)
+                                                                        <td>11</td>
+                                                                        @break
+                                                                    @case(38)
+                                                                        <td>12</td>
+                                                                        @break
+                                                                    @case(40)
+                                                                        <td>13</td>
+                                                                        @break
+                                                                    @case(42)
+                                                                        <td>14</td>
+                                                                        @break
+                                                                    @default
+                                                                        <td>14</td>
+                                                                @endswitch
+                                                                {{-- <td>{{ $bookedClass->bike }}</td> --}}
                                                                 @if($bookedClass->status == 'active')
                                                                     <td>Activo</td>
                                                                 @endif
