7d917861aac22f9761dd7c32ec1f8f5bcd290339
Paul Palemon Moran Garcia <paulpalemon@MacBook-Pro-de-Sergio.local>
2019-07-17 10:56:16 -0500

    fixed error:if a user purchased a deleted product, the User view will throw an error

--- app/userSchedule.php
+++ app/userSchedule.php
@@ -5,11 +5,14 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class UserSchedule extends Model
+
 {
     protected $fillable = [
         'user_id', 'schedule_id', 'purchase_id', 'status', 'bike', 'changedSit',
     ];
-
+    public function scheduleWithTrashed(){
+        return $this->hasOne(Schedule::class, "id", "schedule_id")->withTrashed();
+    }
     public function schedule(){
         return $this->hasOne(Schedule::class, "id", "schedule_id");
     }
