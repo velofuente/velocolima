f449ab3909c144464de7949ed5e7dd5a477f2b01
pablo_kraken_95 <pjimenez@witann.com>
2019-10-25 13:30:10 -0400

    last commit before testing

--- resources/views/emails/birthday-free-class.blade.php
+++ resources/views/emails/birthday-free-class.blade.php
@@ -9,7 +9,7 @@
     <img src="http://velo.witann.com/img/birth_balloon.png" height="200" width="400">
     <h1 style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#2f3133;font-size:19px;font-weight:bold;margin-top:0;text-align:left">¡Feliz cumpleaños {{ $name }}!</h1>
     <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">Queremos festejar contigo, por eso te regalamos una clase gratis.</p>
-    <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">Podrás hacerla valida antes de que transcurran 48 horas.</p>
+    <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">Podrás hacerla válida antes de que transcurran 48 horas.</p>
     <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">Saludos de parte del equipo de Vèlo.</p>
 </body>
 </html>
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -61,7 +61,7 @@
                             <p class="available_classes mb-0">{{$classes}}</p>
                         @endif
                         <span class="classes_message">Clases disponibles en tu cuenta</span>
-                        <a href="{{ url('/schedule') }}" class="btn gradient_button mx-auto" id="bookClass" role="button">Reservar</a>
+                        <a href="{{ url('/schedule') }}" class="btn gradient_button mx-auto" id="bookClass" role="button" style="width: 275px;height: 45px;">Reservar</a>
                         <a href="{{ url('/schedule#packages') }}" class="btn gradient_button mx-auto" id="buyPackages" role="button">Comprar clases</a>
                     </div>
                     {{-- Change User Data & Password --}}
