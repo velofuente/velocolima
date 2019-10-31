bddec550b56264729b8aa4adc7744163d4b1cfe5
antonio misael jacobo alcaraz <mjacobo@witann.com>
2019-10-31 13:58:03 -0600

    modificando el diseño del correo velo

--- resources/views/emails/birthday-free-class.blade.php
+++ resources/views/emails/birthday-free-class.blade.php
@@ -5,13 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
-<body style="background-color: #424040d1;">
-  
-    <img src="/img/birth_balloon.png" height="200" width="400">
-    <h1 style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:white;font-size:19px;font-weight:bold;margin-top:0;text-align:left">¡Feliz cumpleaños {{ $name }}!</h1>
-    <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:white;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">Queremos festejar contigo, por eso te regalamos una clase gratis.</p>
-    <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:white;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">Podrás hacerla válida antes de que transcurran 48 horas.</p>
-    <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:white;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">Saludos de parte del equipo de Vèlo.</p>
-    <img src="/img/iconos/LOGO.png" height="150" width="300">
+<body >
+    <center>
+        <table width="50%" border="0" cellpadding="0" cellspacing="0" bgcolor="#7d7c7a">
+            <tr>
+                <td align="center" valign="top">
+                    <img src="{{URL::asset('/img/birth_balloon.png')}}" height="200" width="400">
+                </td>
+            </tr>
+            <tr>
+                <td align="center" valign="top">
+                    <h1 style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:white;font-size:19px;font-weight:bold;margin-top:0;text-align:center">¡Feliz cumpleaños !</h1>
+                    <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:white;font-size:16px;line-height:1.5em;margin-top:0;text-align:center">Queremos festejar contigo, por eso te regalamos una clase gratis.</p>
+                    <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:white;font-size:16px;line-height:1.5em;margin-top:0;text-align:center">Podrás hacerla válida antes de que transcurran 48 horas.</p>
+                    <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:white;font-size:16px;line-height:1.5em;margin-top:0;text-align:center">Saludos de parte del equipo de Vèlo.</p>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" valign="bottom">
+                    <img src="{{URL::asset('/img/iconos/LOGO.png')}}" height="150" width="300"> 
+                </td>
+            </tr>
+        </table>
+    </center>
 </body>
 </html>
