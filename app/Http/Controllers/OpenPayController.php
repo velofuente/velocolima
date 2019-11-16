90f92846f28a4a292c6645c83ce37e39679537a4
antonio misael <mjacobo@witann.com>
2019-11-15 23:41:27 -0600

    agregando la verificacion de promocion cuando se compra un paquete de 10

--- app/Http/Controllers/OpenPayController.php
+++ app/Http/Controllers/OpenPayController.php
@@ -242,15 +242,35 @@ class OpenPayController extends Controller
             // $purchaseArray["card_id"] = $card->id;
             // $cardToken = $card->token_id;
             //Registramos la compra en el sistema
-            $compra = Purchase::create($purchaseArray);
             //promocion clase adicional
-            $promotion = Purchase::where('user_id', $requestUser->id)->where('status', 'pending')->latest()->first();
+            /*$promotion = Purchase::where('user_id', $requestUser->id)->where('status', 'pending')->latest()->first();
             if($promotion != null){
                 if(Carbon::now() < Carbon::parse($promotion->created_at)->addDay() && $product->n_classes >= 10){
                     $promotion->status = 'active';
                     $promotion->save;
                 }
+            }*/
+            //verificar si compro un paquete de mas o igual a 10 clases
+            if(intval($product->n_classes) >= 10){
+                //promocion clase adicional verificar si tiene 1 clase
+                $lastClassPurchase = Purchase::where('user_id', $requestUser->id)
+                ->where('n_classes', "<>", 0)
+                ->whereRaw("NOW() < DATE_ADD(created_at, INTERVAL expiration_days DAY)")
+                ->orderByRaw('DATE_ADD(created_at, INTERVAL expiration_days DAY)')->first();
+                if($lastClassPurchase){
+                    if($lastClassPurchase->product->id != 1 ||  $lastClassPurchase->product->id != 11){
+                        $promocion = Product::find(12);
+                        Purchase::create([
+                            'product_id' => $promocion->id,
+                            'user_id' => $requestUser->id,
+                            'n_classes' => $promocion->n_classes,
+                            'expiration_days' => $promocion->expiration_days,
+                            'status' => 'active',
+                        ]);
+                    }
+                }
             }
+            $compra = Purchase::create($purchaseArray);
             log::info("crea la compra");
             //Inicializamos array de cargo (OpenPay)
             $chargeData = [
